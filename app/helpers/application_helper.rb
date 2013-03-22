b46bc021dd1dc5c5b0f98ba1860a1bf2cac7a665
amaierhofer <a.maierhofer@gmail.com>
2013-03-22 22:47:03 +0100

    stack as circles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,5 +43,12 @@ module ApplicationHelper
     end
   end
 
+  # use => because we already poped stack when we are viewing this
+  def stack_circles
+    classes = Settings.needed_for_win.times.map { |index| @stack.size >= index ? :me : :you }
+    content = classes.map { |css_class| content_tag(:div, "", class: "circle #{css_class}") }
+    content_tag(:div, safe_join(content), class: 'circles')
+  end
+
 
 end
